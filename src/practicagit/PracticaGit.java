6e06d8db4042baf47736a1650884391ff377bf6b
edher92 <edher_diazfdz@outlook.com>
2017-11-14 15:19:52 -0600

    agregar

---
+++ src/practicagit/PracticaGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicagit;
+
+/**
+ *
+ * @author edher
+ */
+public class PracticaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
